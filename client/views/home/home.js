b710355caefcfe1dcaf6e3b9c62227184c5c63c4
TheEndurance <rawa.jalal@gmail.com>
2018-12-31 14:03:40 -0500

    Minor UI changes/Rough portfolio snapshots

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -1,3 +1,12 @@
 import { Template } from 'meteor/templating';
 import './home.css';
 import './home.html';
+
+
+Template.portfolioList.onCreated(function(){
+});
+
+
+Template.portfolioList.helpers({
+
+});
--- server/pubs/Dashboard.js
+++ server/pubs/Dashboard.js
@@ -2,38 +2,37 @@ import { Meteor } from "meteor/meteor";
 import { Portfolios } from "../../collections/Portfolios";
 
 // Aggregation for transactions
-db.portfolios.aggregate([
-    { $match: { $and: [{ name: "TFSA" }, {'transactions.type' : 'watch'}] } },
-    {
-        $group: { _id: "$name", transactions: { $first: '$transactions' } }
-    },
-    {
-        $project: {
-            transactions: {
-                $filter: {
-                    input: '$transactions',
-                    as: 'transactions',
-                    cond: {
-                        $or: [
-                            { $eq: ['$$transactions.type', 'buy'] },
-                            { $eq: ['$$transactions.type', 'sell'] }
-                        ]
-                    }
-                }
-            },
-            _id: 1
-        }
-    },
-]);
+// db.portfolios.aggregate([
+//     { $match: { $and: [{ name: "TFSA" }, { 'transactions.type': 'watch' }] } },
+//     {
+//         $group: { _id: "$name", transactions: { $first: '$transactions' } }
+//     },
+//     {
+//         $project: {
+//             transactions: {
+//                 $filter: {
+//                     input: '$transactions',
+//                     as: 'transactions',
+//                     cond: {
+//                         $or: [
+//                             { $eq: ['$$transactions.type', 'buy'] },
+//                             { $eq: ['$$transactions.type', 'sell'] }
+//                         ]
+//                     }
+//                 }
+//             },
+//             _id: 1
+//         }
+//     },
+// ]);
 
 
 Meteor.publish('portfolioSnapshot', function () {
-    const quantityBySymbol = {};
+    const portfolios = [];
 
     const pipeline = [
         {
-            '$match':
-                { 'transactions.type': 'buy' }
+            $group: { _id: "$name", transactions: { $first: '$transactions' } }
         },
         {
             $project: {
@@ -49,7 +48,7 @@ Meteor.publish('portfolioSnapshot', function () {
                         }
                     }
                 },
-                _id: 0
+                _id: 1
             }
         },
         {
@@ -58,16 +57,20 @@ Meteor.publish('portfolioSnapshot', function () {
             }
         }
     ]
-    Portfolios.rawCollection().aggregate(pipeline).transactions.forEach(function (transaction) {
-        const multiplier = transaction.type === 'buy' ? 1 : -1;
-        if (quantityBySymbol[transaction.symbol]) {
-            quantityBySymbol[transaction.symbol] += (transaction.quantity * multiplier);
-        } else {
-            quantityBySymbol[transaction.symbol] = transaction.quantity;
-        }
+    Portfolios.rawCollection().aggregate(pipeline).forEach(function (portfolio) {
+        if (!portfolio.transactions || portfolio.transactions.length === 0) return;
+        const currPortfolio = { name: portfolio._id };
+        const quantityBySymbol = {};
+        const sum = portfolio.transactions.reduce(function (sum,transaction) {
+            const multiplier = transaction.type === 'buy' ? 1 : -1;
+        },[]);
+        currPortfolio.quantityBySymbol = quantityBySymbol;
+        portfolios.push(currPortfolio);
     });
 
-    const portfolioTotal =
+    portfolios.forEach(function (portfolio) {
+
+    });
 
     //maybe I can get the quantity and symbols of all transactions that are 'buy'
     // and all transactions that are 'sell'
@@ -75,3 +78,22 @@ Meteor.publish('portfolioSnapshot', function () {
 
     //make query to Last stock price collection for only the symbols we are interested in based on the portfolio
 });
+
+
+
+
+// Portfolios.rawCollection().aggregate(pipeline).forEach(function (portfolio) {
+//     if (!portfolio.transactions || portfolio.transactions.length === 0) return;
+//     const currPortfolio = { name: portfolio._id };
+//     const quantityBySymbol = {};
+//     portfolio.transactions.forEach(function (transaction) {
+//         const multiplier = transaction.type === 'buy' ? 1 : -1;
+//         if (quantityBySymbol[transaction.symbol]) {
+//             quantityBySymbol[transaction.symbol] += (transaction.quantity * multiplier);
+//         } else {
+//             quantityBySymbol[transaction.symbol] = transaction.quantity;
+//         }
+//     });
+//     currPortfolio.quantityBySymbol = quantityBySymbol;
+//     portfolios.push(currPortfolio);
+// });
