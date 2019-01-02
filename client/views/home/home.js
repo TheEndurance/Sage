85dc85b9699bf86291ac87742c56345a8a788415
TheEndurance <rawa.jalal@gmail.com>
2019-01-02 15:16:21 -0500

    Move IEX data from server to client instead

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -1,12 +1,93 @@
 import { Template } from 'meteor/templating';
+import io from 'socket.io-client';
 import './home.css';
 import './home.html';
 
+//collections
+import { Portfolios } from '../../../collections/Portfolios.js';
+import { Last } from '../../../collections/IEX.js'
 
-Template.portfolioList.onCreated(function(){
+//IEX Websocket endpoint
+const lastURL = 'https://ws-api.iextrading.com/1.0/last';
+const socket = io(lastURL);
+//IEX Http endpoints
+const ohlcURL = (symbol) => `https://api.iextrading.com/1.0/stock/${symbol}/ohlc`;
+
+
+/* #region Home parent template */
+Template.home.onCreated(function () {
+    const tpl = this;
+    //subscriptions
+    const portfoliosHandle = this.subscribe('get:portfolios');
+
+    //live stock data
+    socket.on('connect', () => {
+        tpl.autorun(() => {
+            if (portfoliosHandle.ready()) {
+                const portfolios = Portfolios.find({}).fetch();
+                const symbols = {};
+                portfolios.forEach((portfolio) => {
+                    if (!portfolio.transactions || portfolio.transactions.length === 0) return;
+                    portfolio.transactions.forEach((transaction) => {
+                        if (symbols[transaction.symbol]) return;
+                        symbols[transaction.symbol] = true;
+                    });
+                });
+                const symbolsString = Object.keys(symbols).reduce((accum, current, index) => {
+                    return index === 0 ? current : accum + ',' + current;
+                }, '');
+                socket.emit('subscribe', symbolsString);
+            }
+        });
+    });
+
+    //Live IEX data that updates the local collections or inserts if there is none
+    socket.on('message', async (message) => {
+        const { symbol, price } = JSON.parse(message);
+        let update = { symbol: symbol, price: price };
+
+        const exists = Last.findOne({ symbol: symbol });
+
+        if (!exists) {
+            const response = await fetch(ohlcURL(symbol));
+            const { close } = await response.json();
+            update.closePrice = close.price;
+        }
+
+        Last.update({ symbol: symbol },
+            update,
+            { upsert: true });
+    });
+});
+
+Template.home.onDestroyed(function () {
+    socket.disconnect();
+});
+
+Template.home.helpers({
+    portfolioListData() {
+        const instance = Template.instance();
+        return {
+            portfoliosReady: instance.subscriptionsReady(),
+            portfolios() {
+                return Portfolios.find({});
+            },
+            livePrices() {
+                return Last.find({});
+            }
+        }
+    }
+});
+/* #endregion */
+
+
+/* #region Portfolio List */
+Template.portfolioList.onCreated(function () {
+
 });
 
 
 Template.portfolioList.helpers({
 
 });
+/* #endregion */
--- server/pubs/Dashboard.js
+++ server/pubs/Dashboard.js
@@ -28,49 +28,44 @@ import { Portfolios } from "../../collections/Portfolios";
 
 
 Meteor.publish('portfolioSnapshot', function () {
-    const portfolios = [];
+    // const portfolios = [];
 
-    const pipeline = [
-        {
-            $group: { _id: "$name", transactions: { $first: '$transactions' } }
-        },
-        {
-            $project: {
-                transactions: {
-                    $filter: {
-                        input: '$transactions',
-                        as: 'transactions',
-                        cond: {
-                            $or: [
-                                { $eq: ['$$transactions.type', 'buy'] },
-                                { $eq: ['$$transactions.type', 'sell'] }
-                            ]
-                        }
-                    }
-                },
-                _id: 1
-            }
-        },
-        {
-            $group: {
-                _id: 1
-            }
-        }
-    ]
-    Portfolios.rawCollection().aggregate(pipeline).forEach(function (portfolio) {
-        if (!portfolio.transactions || portfolio.transactions.length === 0) return;
-        const currPortfolio = { name: portfolio._id };
-        const quantityBySymbol = {};
-        const sum = portfolio.transactions.reduce(function (sum,transaction) {
-            const multiplier = transaction.type === 'buy' ? 1 : -1;
-        },[]);
-        currPortfolio.quantityBySymbol = quantityBySymbol;
-        portfolios.push(currPortfolio);
-    });
+    // const pipeline = [
+    //     {
+    //         $group: { _id: "$name", transactions: { $first: '$transactions' } }
+    //     },
+    //     {
+    //         $project: {
+    //             transactions: {
+    //                 $filter: {
+    //                     input: '$transactions',
+    //                     as: 'transactions',
+    //                     cond: {
+    //                         $or: [
+    //                             { $eq: ['$$transactions.type', 'buy'] },
+    //                             { $eq: ['$$transactions.type', 'sell'] }
+    //                         ]
+    //                     }
+    //                 }
+    //             },
+    //             _id: 1
+    //         }
+    //     },
+    // ]
+    // Portfolios.rawCollection().aggregate(pipeline).forEach(function (portfolio) {
+    //     if (!portfolio.transactions || portfolio.transactions.length === 0) return;
+    //     const currPortfolio = { name: portfolio._id };
+    //     const quantityBySymbol = {};
+    //     const sum = portfolio.transactions.reduce(function (sum,transaction) {
+    //         const multiplier = transaction.type === 'buy' ? 1 : -1;
+    //     },[]);
+    //     currPortfolio.quantityBySymbol = quantityBySymbol;
+    //     portfolios.push(currPortfolio);
+    // });
 
-    portfolios.forEach(function (portfolio) {
+    // portfolios.forEach(function (portfolio) {
 
-    });
+    // });
 
     //maybe I can get the quantity and symbols of all transactions that are 'buy'
     // and all transactions that are 'sell'
